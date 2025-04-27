8ff0517c920c7793bc9f5e3bf52432a8833abd63
mrcantabridge <mrcantabridge@gmail.com>
2025-04-27 09:16:58 -0400

    fixed error with nlbs

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 // main.js
-import { solid } from "kaboom";
-window.buildTime = new Date().toLocaleString();
 
 // barrierSize needs kaboom() first, so we set it now:
 window.barrierSize = vec2(10, 10);
@@ -12,12 +10,16 @@ add([
   outline(4),
   color(50, 50, 50),
 ]);
+
+// Load sprites
 loadSprite("creatureFront", "sprites/front.png");
 loadSprite("creatureLeft", "sprites/left.png");
 loadSprite("creatureRight", "sprites/right.png");
-
 
 // Spawn initial animals
 for (let i = 0; i < 5; i++) {
   spawnAnimal();
 }
+
+// Now setup the UI
+setupUI();
